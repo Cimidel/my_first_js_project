01197c88740c4605aae3f1e0bc0617a5a1c28eac
davidbacter01 <davidbacter01@gmail.com>
2024-03-04 19:14:05 +0200

    Add addition example

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ console.log(age);
 age = 36;
 console.log(age);
 
+const piNumber = 3.14;
+console.log(piNumber);
+
 let userName = 'David';
 console.log('name: ');
 console.log(userName);
+
+// Operatori date numerice
+
+// adunare
+let firstNumber = 10;
+firstNumber = 12;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
 
