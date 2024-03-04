df51e9a97a547d76b53b7d2ec1b6844750098223
davidbacter01 <davidbacter01@gmail.com>
2024-03-04 19:52:06 +0200

    Add string length and index examples

--- script.js
+++ script.js
@@ -54,3 +54,16 @@ console.log(Number.MIN_SAFE_INTEGER);
 // numere de tip BigInt
 let bigIntNumber = 12491241924819n;
 console.log(`BigInt value is ${bigIntNumber}`);
+
+
+// String operations
+const myName = 'David Bacter';
+
+// afisarea unui caracter de la o pozitie
+// primul caracter
+const firstCharInName = myName[0];
+console.log(`Primul caracter din sirul '${myName}' este ${firstCharInName}`);
+
+// lungimea sirului
+const myNameLength = myName.length;
+console.log(`Lungimea sirului '${myName}' este ${myNameLength}`);
